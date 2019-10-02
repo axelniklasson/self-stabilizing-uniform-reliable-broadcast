e961d55ea7523e059eb0383fabea9ff4d58f9b8f
Axel Niklasson <axel.niklasson@live.com>
2019-10-02 21:41:22 +0200

    improved dist client script

--- scripts/client_distributed.sh
+++ scripts/client_distributed.sh
@@ -1,9 +1,9 @@
 # make sure to run this from the root of the project directory
 #
-# ./scripts/client_distributed.sh MSG_COUNT HOSTS_PATH
+# ./scripts/client_distributed.sh MSG_COUNT CLIENT_COUNT HOSTS_PATH
 
 if [ $# -lt 2 ]; then
-    echo 1>&2 "$0: not enough arguments, run as ./scripts/client_distributed.sh MSG_COUNT HOSTS_PATH"
+    echo 1>&2 "$0: not enough arguments, run as ./scripts/client_distributed.sh MSG_COUNT CLIENT_COUNT HOSTS_PATH"
     exit 2
 fi
 
@@ -15,8 +15,10 @@ splitAndGet () {
 }
 
 MSG_COUNT=$1
-HOSTS_PATH=$2
+CLIENT_COUNT=$2
+HOSTS_PATH=$3
 
+LAUNCHED_CLIENTS=0
 for l in $(cat $HOSTS_PATH)
 do
     ID=$(splitAndGet $l ',' 0)
@@ -25,4 +27,9 @@ do
     PORT=$((4000+$ID))
     curl -d '{"reqCount": '$MSG_COUNT'}' -H "Content-Type: application/json" -X POST http://$HOSTNAME:$PORT/client/launch
     echo "Launched client that will inject $MSG_COUNT messages on $HOSTNAME"
+
+    LAUNCHED_CLIENTS=$(( $LAUNCHED_CLIENTS + 1 ))
+    if [ $LAUNCHED_CLIENTS == $CLIENT_COUNT ]; then
+        exit
+    fi
 done
